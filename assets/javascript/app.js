603d0834e44cc35f814ddcc8992e29a1c13336b3
rkrug5 <phishermankrug@hotmail.com>
2018-02-16 17:33:17 -0700

    cleaning up

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,6 +6,24 @@ var lossCounter = 0;
 
 
 
+
+var randomNumberGem1 = Math.floor(Math.random()*11+1);
+console.log(randomNumberGem1);
+var randomNumberGem2 = Math.floor(Math.random()*11+1);
+console.log(randomNumberGem2);
+var randomNumberGem3 = Math.floor(Math.random()*11+1);
+console.log(randomNumberGem3);
+var randomNumberGem4 = Math.floor(Math.random()*11+1);
+console.log(randomNumberGem4);
+
+function winrar() {
+    alert("You won!");
+}
+
+function loser() {
+    alert("Sorry, you lost.");
+}
+
 $(document).ready(function() {
 
 
@@ -13,25 +31,25 @@ $(document).ready(function() {
 
 $("#gem1").on("click", function() {
         
-        currentNumber +=  Math.floor(Math.random()*11+1);
+        currentNumber +=  randomNumberGem1;
 		document.getElementById("currentNumberSpot").innerHTML = currentNumber;
         checkWinCondition();
     });
 $("#gem2").on("click", function() {
         
-        currentNumber += Math.floor(Math.random()*11+1);
+        currentNumber += randomNumberGem2;
         document.getElementById("currentNumberSpot").innerHTML = currentNumber;
         checkWinCondition();
     });
 $("#gem3").on("click", function() {
         
-        currentNumber += Math.floor(Math.random()*11+1);
+        currentNumber += randomNumberGem3;
         document.getElementById("currentNumberSpot").innerHTML = currentNumber;
         checkWinCondition();
     });
 $("#gem4").on("click", function() {
         
-        currentNumber += Math.floor(Math.random()*11+1);
+        currentNumber += randomNumberGem4;
 		document.getElementById("currentNumberSpot").innerHTML = currentNumber;
         checkWinCondition();
 
@@ -48,15 +66,16 @@ document.getElementById("winCounter").innerHTML = "Wins: " + winCounter;
 
 
 function checkWinCondition () {
+
 	if (randomNumber == currentNumber) {
 
-		alert("You Win!");
+        setTimeout(winrar, 50);
 		winCounter++;
 		document.getElementById('winCounter').innerHTML = "Wins: " + winCounter;
 	}
 
 	if (currentNumber > randomNumber) {
-		alert("You Lost!");
+		setTimeout(loser, 50);
 		lossCounter++;
 		document.getElementById('lossCounter').innerHTML = "Losses: " + lossCounter;
 	}
@@ -65,16 +84,23 @@ function checkWinCondition () {
 
   $("#resetButton").on("click", function() {
       	currentNumber = 0;
-      	randomNumber = Math.floor(Math.random()*100+1);
+        randomNumber = Math.floor(Math.random()*101+19);
+          
+        console.log("--------------------------------");
+        console.log(randomNumber);
      	document.getElementById('currentNumberSpot').innerHTML = currentNumber;
 		document.getElementById('targetNumber').innerHTML = randomNumber;
-		document.getElementById('lossCounter').innerHTML = "Losses: " + lossCounter;
-		document.getElementById('winCounter2').innerHTML = "Wins: " + winCounter;
+	
+        randomNumberGem1 = Math.floor(Math.random()*11+1);
+        console.log(randomNumberGem1);
+        randomNumberGem2 = Math.floor(Math.random()*11+1);
+        console.log(randomNumberGem2);
+        randomNumberGem3 = Math.floor(Math.random()*11+1);
+        console.log(randomNumberGem3);
+        randomNumberGem4 = Math.floor(Math.random()*11+1);
+        console.log(randomNumberGem4);
       
   });
-
-        
-        
 
 
 });
